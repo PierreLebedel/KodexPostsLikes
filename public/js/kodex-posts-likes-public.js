314a45a872ffaefa367c54068b8cdcb10d04ecec
Pierre Lebedel <pierrelebedel@gmail.com>
2016-04-30 11:45:49 +0200

    fixes ajax loaded, fixes #2

--- public/js/kodex-posts-likes-public.js
+++ public/js/kodex-posts-likes-public.js
@@ -1,7 +1,6 @@
 jQuery(function($){
 
-if( $('.kodex_buttons').length && $('.kodex_buttons .kodex_button').length ){
-	$('.kodex_buttons').on('click', '.kodex_button', function(e){
+	$('body').on('click', '.kodex_buttons .kodex_button', function(e){
 		e.preventDefault();
 
 		var button     = $(this);
@@ -13,20 +12,19 @@ if( $('.kodex_buttons').length && $('.kodex_buttons .kodex_button').length ){
 
 		button.addClass('kodex_button_loading');
 		var xhr = $.ajax({
-			url: kodex_posts_likes.ajaxurl, 
-			method: 'post',
-			data: {
+			url    : kodex_posts_likes.ajaxurl, 
+			method : 'post',
+			data   : {
 				action     : 'kodex_posts_likes_ajax',
 				post_id    : post_id,
 				nonce      : nonce,
 				btn_action : btn_action
 			}, 
-			success: function(response){
+			success : function(response){
 				button.removeClass('kodex_button_loading');
 				var wrappers = $('.kodex_button[data-id="'+post_id+'"]').parents('.kodex_buttons').html(response);
 			}
 		});
 	});
-}
 
 });
